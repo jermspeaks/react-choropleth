c73a944f17f5269c19f149b22c93f92d57f29d77
jermspeaks <jermspeaks@gmail.com>
2016-02-24 12:00:07 -0800

    random choropleth with labels

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,34 +1,26 @@
 require('normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React, {Component, PropTypes} from 'react';
 import d3 from 'd3';
 import topojson from 'topojson';
 
 // let us = require('../../data/us.json');
-// let unemployment = require('../../data/unemployment.tsv');
-let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends Component {
   constructor() {
     super();
     this.state = {
-      // counties: [],
       states: [],
       rateById: d3.map()
     };
   }
 
   componentWillMount() {
-    // let unemploymentUrl = 'https://gist.githubusercontent.com/pleasetrythisathome/9713092/raw/5cc6f9b62306555ac8a51c89cbc0ba1b8d38649f/unemployment.tsv';
-    let usJSONUrl = 'https://gist.githubusercontent.com/jermspeaks/fac1ed95e1728fbd1fbe/raw/ddb7df44c99286b5f1bd2394d20317fd7683c8f8/us.json';
-    // d3.tsv(unemploymentUrl, data => {
-    //   this.state.rateById.set(data.id, +data.rate);
-    // });
+    let usJSONUrl = 'https://gist.githubusercontent.com/jermspeaks/fac1ed95e1728fbd1fbe/raw/c340b4da7131fe687fc7e89bb9630e6dfba9921f/states.json';
 
     d3.json(usJSONUrl, usData => {
       this.setState({
-        // counties: topojson.feature(usData, usData.objects.counties).features,
         states: topojson.feature(usData, usData.objects.states).features
       });
     })
@@ -46,7 +38,6 @@ class AppComponent extends Component {
 
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
         <svg
           className='choropleth Blues'
           width={this.props.width}
@@ -55,12 +46,22 @@ class AppComponent extends Component {
           <g className='state'>
             {this.state.states.map((state, stateIndex) => {
               const randomInt = getRandomInt(0, 8);
+              const pathGenerated = pathGenerator(state);
               return (
-                <path
-                  key={stateIndex}
-                  className={quantize(randomInt)}
-                  d={pathGenerator(state)}
-                />
+                <g key={stateIndex}>
+                  <path
+                    className={quantize(randomInt)}
+                    d={pathGenerated}
+                  />
+                  <text
+                    x={pathGenerator.centroid(state)[0]}
+                    y={pathGenerator.centroid(state)[1]}
+                    fontFamily="sans-serif"
+                    textAnchor="middle"
+                    fontSize="10px"
+                    fill="black"
+                  >{state.properties.initials}</text>
+                </g>
               )
             })}
           </g>
@@ -69,6 +70,10 @@ class AppComponent extends Component {
     );
   }
 }
+
+// x={pathGenerator.centroid(state)[0]}
+// y={pathGenerator.centroid(state)[1]}
+
 
 AppComponent.propTypes = {
   width: PropTypes.number.isRequired,
